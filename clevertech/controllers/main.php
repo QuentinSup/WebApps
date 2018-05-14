631ec575cf5ea9d9e54b73e66e92f830ff8413e1
QuentinSup <qsupernant@gmail.com>
2018-05-14 16:27:37 +0200

    Site internet http://clevertech-it.fr

---
+++ clevertech/controllers/main.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace clevertech;
+
+use dw\classes\dwHttpRequest;
+use dw\classes\dwHttpResponse;
+use dw\classes\dwModel;
+use dw\classes\controllers\dwBasicController;
+
+/**
+ * @Mapping(value = '/')
+ */
+class main extends dwBasicController {
+
+	
+	/**
+	 * @Session()
+	 */
+	public static $session;
+	
+	
+	private function prepareModel(dwHttpRequest $request, dwModel $model, $pageId) {
+		$model -> title = "Clevertech";
+		$model -> pageId = $pageId;
+		$model -> version = "1.0.0";
+		$model -> host = $request -> getBaseUri();
+		return $this;
+	}
+	
+	
+	private function view() {
+		return 'view:./resources/index.html';
+	}
+	
+	/**
+	 * @Mapping(method = "get")
+	 */
+	public function root(dwHttpRequest &$request, dwHttpResponse &$response, dwModel &$model) 
+	{			
+		return $this -> prepareModel($request, $model, 'main') -> view();
+	}
+	
+}
+
+?>
